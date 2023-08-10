e914b99bb2d388a29931d9347c7a87bf7b1ce975
cosmodesant49 <emilmaldybaev49@gmail.com>
2023-08-10 15:49:08 +0600

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double operation;
+        System.out.println("введите первое число");
+        Scanner scanner1 = new Scanner(System.in);
+        double number1 = scanner1.nextDouble();
+        System.out.println("введите первое число");
+        Scanner scanner2 = new Scanner(System.in);
+        double number2 = scanner2.nextDouble();
+        System.out.println("выберите действие (+, -, *, /)");
+        Scanner scanner3 = new Scanner(System.in);
+        char opera   = scanner3.next().charAt(0);
+        switch (opera){
+            case    '+' :
+                System.out.println(operation=number1+number2);
+            break;
+            case    '-' :
+                System.out.println(operation=number1-number2);
+            break;
+            case    '*' :
+                System.out.println(operation=number1*number2);
+            break;
+            case    '/' :
+                System.out.println(operation=number1/number2);
+            break;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
         }
     }
 }
